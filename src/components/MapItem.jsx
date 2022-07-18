da768416c3dd8807d86d82d351932b239e1eb192
casey-web-angel <lucasey1021@gmail.com>
2022-07-18 02:12:52 +0100

    adjusted changes

--- src/components/MapItem.jsx
+++ src/components/MapItem.jsx
@@ -14,7 +14,7 @@ import React from 'react'
 const MapItem = ({ inverse, id, title, desc }) => {
   return (
     <div className={`mx-auto relative flex flex-row items-center ml-6 md:ml-0 justify-between ${inverse ? "flex-row-reverse" : "flex-row"}`}>
-      <img src="/img/dummy.png" className={`w-10 md:w-12 absolute -left-6 md:left-[47.5%] top-0`} data-aos='fade-up'  data-aos-duration='800'/>
+      <img src="/img/dummy.png" className={`w-10 md:w-12 absolute -left-6 md:left-[47.5%] top-0`} data-aos='fade-up'  data-aos-duration='1000'/>
       <div className={`md:basis-[50%]  py-3`} data-aos='fade-up'  data-aos-duration='800'>
         <div className="bg-[#7387D0] text-left w-full max-w-[92%] md:max-w-[90%] mx-auto rounded-md p-4 md:p-6">
           <h1 className="text-white text-xl font-semibold font-irish" >{id} {title}</h1>
