c3f8846c503da38f5d27b7d154f8b22aad1e21ea
casey-web-angel <lucasey1021@gmail.com>
2022-07-18 10:35:58 +0100

    added changes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,7 +3,7 @@ import Navbar from '../layout/Navbar'
 
 function Hero() {
   return (
-    <div id="home" className="h-[90vh] relative w-screen bg-hero_mobile md:bg-hero bg-no-repeat relative  md:bg-bottom bg-center  bg-cover">
+    <div id="home" className="h-screen relative w-screen bg-hero_mobile md:bg-hero bg-no-repeat relative  md:bg-bottom bg-center  bg-cover">
       <Navbar />
       <img src="/img/center.png" data-aos='fade-up'  data-aos-duration='700' className="w-44 left-[45%] hidden lg:block top-[27%] absolute" />
       <img src="/img/left.png" className="w-44 left-[25%] hidden lg:block top-[40%] absolute" data-aos='fade-left'  data-aos-duration='500' />
@@ -13,9 +13,6 @@ function Hero() {
       <img src="/img/small_left.png" className="w-36 left-0 md:hidden block top-[45%] absolute" data-aos='fade-left'  data-aos-duration='500' />
       <img src="/img/small_right.png" className="w-36 right-0 block md:hidden top-[45%] absolute" data-aos='fade-right'  data-aos-duration='600' />
       <h1  data-aos='fade-left'  className="text-4xl  font-irish justify-center  absolute top-[20%] left-[10%] md:left-[30%]  text-transparent md:my-12 md:text-7xl bg-clip-text font-extrabold bg-gradient-to-b from-[#de5e82] to-[#4577d4] flex items-center">CLOUDBUNNIES</h1>
-      <div className='md:max-w-[25%] max-w-[60%] w-full flex absolute items-center top-[60%] left-[23%] md:top-[65%] mx-auto'  data-aos='fade-up' >
-        <button className="bg-button bg-center py-2 md:py-3 bg-cover w-full text-white uppercase text-lg md:text-xl font-irish border-none rounded-full ">Explore Bunny</button>
-      </div>
     </div>
   )
 }
