6c19112266be01d3a164e4b9109e67639d6526cc
casey-web-angel <lucasey1021@gmail.com>
2022-07-18 12:26:38 +0100

    added changes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,16 +3,16 @@ import Navbar from '../layout/Navbar'
 
 function Hero() {
   return (
-    <div id="home" className="lg:h-[1079px] h-screen relative w-screen bg-hero_mobile lg:bg-hero bg-no-repeat relative  lg:bg-bottom bg-center  bg-cover">
+    <div id="home" className="lg:h-screen h-screen relative w-screen bg-hero_mobile lg:bg-hero bg-no-repeat relative  lg:bg-bottom bg-center  bg-cover">
       <Navbar />
-      <img src="/img/center.png"className="w-[30%] left-[35%] hidden lg:block top-[30%] absolute" />
-      <img src="/img/small_left.png" className="w-[32%] left-0 hidden lg:block top-[50%] absolute object-fit"  />
-      <img src="/img/small_right.png" className="w-[32%] right-0 lg:block hidden top-[50%] absolute object-fit" />
+      <img src="/img/center.png"className="w-[29%] left-[35%] hidden lg:block top-[15%] absolute" />
+      <img src="/img/small_left.png" className="w-[33%] left-0 hidden p-4 lg:block top-[35%] absolute object-fit"  />
+      <img src="/img/small_right.png" className="w-[33%] right-0 lg:block p-4 hidden top-[35%] absolute object-fit" />
       {/* mobile */}
       <img src="/img/center.png"  className="w-40 left-[32%] lg:hidden block top-[34%] absolute" />
       <img src="/img/small_left.png" className="w-40 left-0 lg:hidden block top-[50%] absolute"   />
       <img src="/img/small_right.png" className="w-40 right-0 block lg:hidden top-[50%] absolute"  />
-      <h1  data-aos='fade-left'  className="text-4xl  font-irish justify-center  absolute top-[25%] left-[10%] lg:left-[30%]  text-transparent lg:my-12 lg:text-7xl bg-clip-text font-extrabold bg-gradient-to-b from-[#de5e82] to-[#4577d4] flex items-center">CLOUDBUNNIES</h1>
+      <h1  data-aos='fade-left'  className="text-4xl  font-irish justify-center  absolute top-[25%] lg:top-[15%] left-[10%] lg:left-[30%]  text-transparent lg:my-12 lg:text-7xl bg-clip-text font-extrabold bg-gradient-to-b from-[#de5e82] to-[#4577d4] flex items-center">CLOUDBUNNIES</h1>
     </div>
   )
 }
